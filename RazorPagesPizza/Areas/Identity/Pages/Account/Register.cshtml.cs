a67f36b1437df0fdbf8d1fdcabd0dea1f6f323a6
Ramon <dragonwarrior66.rr@gmail.com>
2024-04-03 10:54:57 +0200

    before startup rewrite

--- RazorPagesPizza/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RazorPagesPizza/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,20 +81,20 @@ namespace RazorPagesPizza.Areas.Identity.Pages.Account
             [Display(Name = "Last name")]
             public string LastName { get; set; }
             
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
-            [Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
+            // /// <summary>
+            // ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            // ///     directly from your code. This API may change or be removed in future releases.
+            // /// </summary>
+            // [Required]
+            // [EmailAddress]
+            // [Display(Name = "Email")]
+            // public string Email { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Required]
+            // [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -128,35 +128,35 @@ namespace RazorPagesPizza.Areas.Identity.Pages.Account
                 user.FirstName = Input.FirstName;
                 user.LastName  = Input.LastName;
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, user.FirstName, CancellationToken.None); //(user, Input.Email, CancellationToken.None)
+                // await _emailStore.SetEmailAsync(user, null, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
-                    _logger.LogInformation("User created a new account with password.");
+                    _logger.LogInformation("User created a new account.");
 
-                    var userId = await _userManager.GetUserIdAsync(user);
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                        protocol: Request.Scheme);
+                    // var userId = await _userManager.GetUserIdAsync(user);
+                    // var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    // code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                    // var callbackUrl = Url.Page(
+                    //     "/Account/ConfirmEmail",
+                    //     pageHandler: null,
+                    //     values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+                    //     protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
+                    // await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    //     $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                    //
+                    // if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                    // {
+                    //     return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
+                    // }
+                    // else
+                    // {
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
-                    }
+                    // }
                 }
                 foreach (var error in result.Errors)
                 {
--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,16 +1,55 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesPizza.Areas.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection")
                        ?? throw new InvalidOperationException(
                            "Connection string 'RazorPagesPizzaAuthConnection' not found.");
 
-builder.Services.AddDbContext<RazorPagesPizzaAuth>(options => options.UseSqlServer(connectionString));
+void ConfigureServices(IServiceCollection services) {
+    services.AddDbContext<RazorPagesPizzaAuth>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true)
-       .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
+    services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
+
+
+    // builder.Services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+    // {
+    //     microsoftOptions.ClientId     = builder.Configuration["Authentication:Microsoft:ClientId"];
+    //     microsoftOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
+    // });
+
+    services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+
+    services.AddControllersWithViews(options => {
+        var policy = new AuthorizationPolicyBuilder()
+                     .RequireAuthenticatedUser()
+                     .Build();
+        options.Filters.Add(new AuthorizeFilter(policy));
+    });
+    services.AddRazorPages()
+            .AddMicrosoftIdentityUI();
+}
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -22,7 +61,7 @@ if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}       
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
