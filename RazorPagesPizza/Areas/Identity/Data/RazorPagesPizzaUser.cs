e473f0c7801bf8a5d160f2620547e92bd8849005
Ramon <dragonwarrior66.rr@gmail.com>
2024-04-03 08:32:11 +0200

    Register page working

--- RazorPagesPizza/Areas/Identity/Data/RazorPagesPizzaUser.cs
+++ RazorPagesPizza/Areas/Identity/Data/RazorPagesPizzaUser.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
 
 namespace RazorPagesPizza.Areas.Identity.Data;
 
 public class RazorPagesPizzaUser : IdentityUser {
-    public string Uid { get; set; }
+    public string Uid { get; set; } = string.Empty;
     
+    [Microsoft.Build.Framework.Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+    
+    [Microsoft.Build.Framework.Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
 }
--- RazorPagesPizza/Program.cs
+++ RazorPagesPizza/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesPizza.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection") ?? throw new InvalidOperationException("Connection string 'RazorPagesPizzaAuthConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("RazorPagesPizzaAuthConnection")
+                       ?? throw new InvalidOperationException(
+                           "Connection string 'RazorPagesPizzaAuthConnection' not found.");
 
 builder.Services.AddDbContext<RazorPagesPizzaAuth>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<RazorPagesPizzaAuth>();
+builder.Services.AddDefaultIdentity<RazorPagesPizzaUser>(options => options.SignIn.RequireConfirmedAccount = true)
+       .AddEntityFrameworkStores<RazorPagesPizzaAuth>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -14,12 +18,11 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
+}       
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
